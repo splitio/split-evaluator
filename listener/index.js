f71fccbc8cd9b5d97b7d8354d9a5fc86eb2cb60d
Matias Melograno <matiasmelograno@split.io>
2019-09-18 19:02:20 -0300

    removed parsed for listener and use impression directly

--- listener/index.js
+++ listener/index.js
@@ -8,18 +8,8 @@ impressionManager.start();
  * @param {Object} impressionData 
  */
 const logImpression = (impressionData) => {
-  const impression = impressionData.impression;
-  const impressionToAdd = {
-    feature: impression.feature,
-    keyName: impression.keyName,
-    treatment: impression.treatment,
-    time: impression.time,
-    changeNumber: impression.changeNumber,
-    label: impression.label,
-  };
-
   // Adds Impression to queue
-  impressionManager.trackImpression(impressionToAdd);
+  impressionManager.trackImpression(impressionData.impression);
 };
 
 module.exports = logImpression;
