9362b4994b3ae0b2ef8dd15f1551dbdbc64e611b
Matias Melograno <matiasmelograno@split.io>
2019-09-23 18:59:14 -0300

    fixed url regex to allow - _

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ openApiDefinition.info.version = utils.getVersion();
 // Puts server url and port
 
 // SWAGGER URL
-const swaggerUrl = process.env.SPLIT_EVALUATOR_SWAGGER_URL ? validUrl('SPLIT_EVALUATOR_SWAGGER_URL') : 'http://localhost';
+const swaggerUrl = process.env.SPLIT_EVALUATOR_SWAGGER_URL ? validUrl('SPLIT_EVALUATOR_SWAGGER_URL') : `http://localhost${process.env.SPLIT_EVALUATOR_SERVER_PORT || 7548}`;
+openApiDefinition.servers = [{url: swaggerUrl}];
 
-openApiDefinition.servers = [{url: `${swaggerUrl}:${process.env.SPLIT_EVALUATOR_SERVER_PORT || 7548}`}];
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openApiDefinition));
 
 // Auth middleware
