6baf4a50a76b04214f6d8a1371824b6aca6536d6
NicoZelaya <zelayadn@gmail.com>
2022-04-12 02:46:45 -0300

    Modify attributes input validation

--- utils/inputValidation/attributes.js
+++ utils/inputValidation/attributes.js
@@ -1,4 +1,4 @@
-const { isObject } = require('../lang');
+const { isObject, isString } = require('../lang');
 const errorWrapper = require('./wrapper/error');
 const okWrapper = require('./wrapper/ok');
 
@@ -8,7 +8,13 @@ const validateAttributes = (maybeAttributes) => {
     return okWrapper(null);
   }
   try {
-    const attributes = JSON.parse(maybeAttributes);
+    let attributes;
+    if (isString(maybeAttributes)) { // If it came as a query param, parse it from string
+      maybeAttributes = JSON.parse(maybeAttributes);
+    } 
+    if (isObject(maybeAttributes)) { // Parsed query or coming from a JSON payload, check if it's an object.
+      attributes = maybeAttributes;
+    }
     return (isObject(attributes)) ? okWrapper(attributes) : errorWrapper('attributes must be a plain object.');
   } catch (e) {
     return errorWrapper('attributes must be a plain object.');
