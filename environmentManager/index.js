2943b17d942e6f0caf3e1d409854c0cb8904d376
Emmanuel Zamora <emmanuel-zamora@split.io>
2023-01-20 15:42:26 -0300

    [SDKS-6474] Initialize evaluator if there is at least one client ready

--- environmentManager/index.js
+++ environmentManager/index.js
@@ -6,24 +6,32 @@ const SPLIT_EVALUATOR_AUTH_TOKEN = 'SPLIT_EVALUATOR_AUTH_TOKEN';
 const SPLIT_EVALUATOR_API_KEY = 'SPLIT_EVALUATOR_API_KEY';
 
 const EnvironmentManagerFactory = (function(){
+  /**
+   * EnvironmentManager singleton
+   */
   class EnvironmentManager {
 
     constructor() {
-
-      // In environments are the apiKey, factory an clientReadiness status related to an authToken
+      // Contains every environment related to an authToken with its apiKey, factory and clientReadiness status
       this._environments = {};
 
       // Ready promises for each client in environment manager
       this._readyPromises = [];
 
+      // Used to know if there is any client ready and evaluator should start
+      this._clientsReady = false;
+
+      // Defines if openApi security tag should be added
       this.requireAuth = true;
 
       this._initializeEnvironments();
     }
 
     _initializeEnvironments(){
+      // If environments envVar is not defined, it creates an environment with auth_token and api_key envVars
       if (!process.env.SPLIT_EVALUATOR_ENVIRONMENTS) {
         const AUTH_TOKEN = process.env.SPLIT_EVALUATOR_AUTH_TOKEN;
+        // If auth_token envVar is not defined, means that openapi security tag should not be added
         if (!AUTH_TOKEN) {
           this.requireAuth = false;
           process.env.SPLIT_EVALUATOR_AUTH_TOKEN = 'splitToken';
@@ -51,6 +59,7 @@ const EnvironmentManagerFactory = (function(){
           throwError(`There are two or more environments with the same authToken '${authToken}' `);
         }
 
+        // Creates an environment for authToken
         this._environments[authToken] = {
           apiKey: apiKey,
           factory: getSplitFactory(settings),
@@ -59,21 +68,27 @@ const EnvironmentManagerFactory = (function(){
 
         const client = this.getFactory(authToken).client();
         this._clientReadiness(client, apiKey);
-
-
       });
-      this.ready();
     }
 
     _clientReadiness(client, apiKey){
+      // Add client ready promise to array to wait asynchronously to be resolved
       this._readyPromises.push(client.ready());
+      // Encode apiKey to log it without exposing it (like ####1234)
       const encodedApiKey = apiKey.replace(/.(?=.{4,}$)/g, '#');
+      // Handle client ready
       client.on(client.Event.SDK_READY, () => {
         console.info(`Client ready for api key ${encodedApiKey}`);
+        this._clientsReady = true;
         client.isClientReady = true;
       });
+      // Handle client timed out
       client.on(client.Event.SDK_READY_TIMED_OUT, () => {
-        throwError(`Client timed out for api key ${encodedApiKey}`);
+        console.info(`Client timed out for api key ${encodedApiKey}`);
+        client.isClientReady = false;
+        client.destroy().then(() => {
+          console.info('Timed out client destroyed');
+        });
       });
     }
 
@@ -103,9 +118,11 @@ const EnvironmentManagerFactory = (function(){
       return Object.keys(this._environments);
     }
 
+    // Awaits for every client ready promise
     async ready() {
-      return Promise.allSettled(this._readyPromises).then(() => {
-        this._clientsReady = true;
+      return Promise.allSettled(this._readyPromises).then((environmentsStatus) => {
+        this._clientsReady = environmentsStatus.some(environment => environment.status === 'fulfilled');
+        return this._clientsReady;
       });
     }
 
@@ -128,6 +145,7 @@ const EnvironmentManagerFactory = (function(){
 
   let instance;
 
+  // Singleton handle strategy
   return {
     hasInstance() {
       return !instance ? false : true;
