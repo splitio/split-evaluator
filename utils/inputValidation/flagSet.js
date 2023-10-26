106f99c8d33e9cf61f016a13c5d650f87456ad61
Emmanuel Zamora <emmanuel-zamora@split.io>
2023-10-26 16:32:48 -0300

    [SDKS-7670] flag set input validation improvements

--- utils/inputValidation/flagSet.js
+++ utils/inputValidation/flagSet.js
@@ -1,19 +1,18 @@
-const { NULL_FLAG_SETS, EMPTY_FLAG_SETS } = require('../constants');
 const errorWrapper = require('./wrapper/error');
 const okWrapper = require('./wrapper/ok');
-const TRIMMABLE_SPACES_REGEX = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/;
+const { NULL_FLAG_SET, EMPTY_FLAG_SET, TRIMMABLE_SPACES_REGEX } = require('../constants');
 
 const validateFlagSet = (maybeFlagSet) => {
   // eslint-disable-next-line
-  if (maybeFlagSet == undefined) return errorWrapper(NULL_FLAG_SETS);
+  if (maybeFlagSet == undefined) return errorWrapper(NULL_FLAG_SET);
 
   if (TRIMMABLE_SPACES_REGEX.test(maybeFlagSet)) {
     console.log(`flag-sets "${maybeFlagSet}" has extra whitespace, trimming.`);
     maybeFlagSet = maybeFlagSet.trim();
   }
-  if (maybeFlagSet.length === 0) return errorWrapper(EMPTY_FLAG_SETS);
+  if (maybeFlagSet.length === 0) return errorWrapper(EMPTY_FLAG_SET);
 
-  return okWrapper(maybeFlagSet.split(','));
+  return okWrapper(maybeFlagSet);
 };
 
 module.exports = validateFlagSet;
