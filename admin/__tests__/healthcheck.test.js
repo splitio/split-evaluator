a48e97e1105480f7822c204386c8a4ea1248ff24
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-12-17 16:02:41 -0300

    Refactor tests for latest Jest

--- admin/__tests__/healthcheck.test.js
+++ admin/__tests__/healthcheck.test.js
@@ -4,26 +4,23 @@ const { expectError } = require('../../utils/testWrapper/index');
 
 describe('healthcheck', () => {
   // Testing authorization
-  test('should be 401 if auth is not passed', async (done) => {
+  test('should be 401 if auth is not passed', async () => {
     const response = await request(app)
       .get('/admin/healthcheck');
     expectError(response, 401, 'Unauthorized');
-    done();
   });
 
-  test('should be 401 if auth does not match', async (done) => {
+  test('should be 401 if auth does not match', async () => {
     const response = await request(app)
       .get('/admin/healthcheck')
       .set('Authorization', 'invalid');
     expectError(response, 401, 'Unauthorized');
-    done();
   });
 
-  test('should be 200', async (done) => {
+  test('should be 200', async () => {
     const response = await request(app)
       .get('/admin/healthcheck')
       .set('Authorization', 'test');
     expect(response.statusCode).toEqual(200);
-    done();
   });
 });
--- admin/__tests__/uptime.test.js
+++ admin/__tests__/uptime.test.js
@@ -4,26 +4,23 @@ const { expectError } = require('../../utils/testWrapper/index');
 
 describe('uptime', () => {
   // Testing authorization
-  test('should be 401 if auth is not passed', async (done) => {
+  test('should be 401 if auth is not passed', async () => {
     const response = await request(app)
       .get('/admin/uptime');
     expectError(response, 401, 'Unauthorized');
-    done();
   });
 
-  test('should be 401 if auth does not match', async (done) => {
+  test('should be 401 if auth does not match', async () => {
     const response = await request(app)
       .get('/admin/uptime')
       .set('Authorization', 'invalid');
     expectError(response, 401, 'Unauthorized');
-    done();
   });
 
-  test('should be 200', async (done) => {
+  test('should be 200', async () => {
     const response = await request(app)
       .get('/admin/uptime')
       .set('Authorization', 'test');
     expect(response.statusCode).toBe(200);
-    done();
   });
 });
